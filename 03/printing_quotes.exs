a6a11d8ae096b32b08df79be19febb240591e2af
Alan Maciel <alan.maciel.salcedo@gmail.com>
2016-10-12 17:08:14 -0500

    Moving clean_string to its own module file

--- 03/printing_quotes.exs
+++ 03/printing_quotes.exs
@@ -8,16 +8,11 @@
 # Alan Maciel says, "Hello World!."
 #
 #
-defmodule Clean do
-  def clean_string(string) do
-    string
-    |> String.trim
-    |> String.replace("\n", "")
-  end
-end
+
+Code.require_file("modules/strings.exs")
 
 quote1 = IO.gets "\nWhat is the quote? "
-quote2 = Clean.clean_string("#{quote1}")
+quote2 = Strings.clean_string("#{quote1}")
 author = IO.gets "\nWho said it? "
-author2 = Clean.clean_string(author)
+author2 = Strings.clean_string(author)
 IO.puts([author2, " says, ", '\"', quote2, '\"'])
