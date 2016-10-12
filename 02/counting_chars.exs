97b27060cbd8a052809e97187880f42bfb204bda
Alan Maciel <alan.maciel.salcedo@gmail.com>
2016-10-11 22:02:46 -0500

    Challenge 2 - Counting numbers of characters, if the user enters nothing, we state that the user needs to enter something to the program.

--- 02/counting_chars.exs
+++ 02/counting_chars.exs
@@ -6,7 +6,12 @@
 # What is the input string? Alan
 # Alan has 4 characters
 #
-answer = IO.gets("\nWhat is the input sting? ") |> String.trim
-char_count = String.length answer
-message =  "\n#{answer} has #{char_count} characters."
+answer = IO.gets("\nWhat is the input string? ") |> String.trim
+message = 
+if answer == "" do
+  "You must enter something into the program."
+else
+  char_count = String.length answer
+  "\n#{answer} has #{char_count} characters."
+end
 IO.puts message
